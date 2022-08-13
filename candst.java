e73b2c4d3d683909bf6dbefcd41dea857ea44069
Barnakghosh07 <101245955+Barnakghosh07@users.noreply.github.com>
2022-08-13 16:57:39 +0530

    Add files via upload

---
+++ candst.java
@@ -0,0 +1,94 @@
+import java.util.*;
+public class basics {
+    public static void main(String[] args) {
+        // int a = 5;
+        // int b = 10;
+
+        // if(a>b) {
+        //     System.out.println("a is bigger than b");
+        // }
+        // else {
+        //     System.out.println("b is bigger than a ");
+        // }
+
+        
+        
+        
+        
+        
+        // Scanner sc = new Scanner(System.in);
+        // int number = sc.nextInt();
+
+        // if(number % 2 == 0){
+        //     System.out.println("The Number is even");
+        // }
+        // else{
+        //     System.out.println("The number is odd");
+        // }
+
+        
+        
+        
+        
+        // Income Tax Calculator
+
+        // Scanner sc = new Scanner(System.in);
+        // int income = sc.nextInt();
+        // int tax;
+
+        // if (income < 500000){
+        //     tax =0;
+        // }
+        // else if (income >= 500000 && income <1000000) {
+        //     tax = (int) (income * 0.2);
+        // }
+        // else {
+        //     tax = (int) (income * 0.3);
+        // }
+        
+        // System.out.println("Your Tax is:" + tax);
+
+        
+        
+        
+        // int number = 4;
+        // String type = ((number%2) == 0) ? "even" : "odd";
+        // System.out.println(type);
+
+        
+        
+        
+        
+        // Scanner sc = new Scanner(System.in);
+        // int marks = sc.nextInt();
+
+        // String RC = marks >= 33 ? "Pass" : "FAil";
+        // System.out.println(RC);
+
+        
+        //Switch Statement
+        
+        
+        
+        // Scanner sc = new Scanner(System.in);
+        // int number = sc.nextInt();
+        // switch(number) {
+        //     case 1 : System.out.println("Burger");
+        //                break;
+        //     case 2 : System.out.println("Roti");
+        //                 break;
+        //     case 3 : System.out.println("Momo");
+        //                 break;
+        // }
+
+
+
+
+        
+
+
+
+
+
+    }
+}
